d4489d60b04ab7f40e02442c6976fbb3bc96823d
Eduardo Braun <eduardo@rushers.org>
2018-05-19 20:44:29 -0300

    .

--- zsh/zsh.d/aliases.zsh
+++ zsh/zsh.d/aliases.zsh
@@ -1,3 +1,5 @@
 alias pacman="sudo pacman"
 alias vim=nvim
 
+alias yy="mpv --really-quiet --volume=50 --autofit=30% --geometry=-10-15 --ytdl --ytdl-format='mp4[height<=?720]' -ytdl-raw-options=playlist-start=1"
+
